755b2736d2f8f2eaafeac9003d3b25dc8110e09d
Alexutzi10 <georgealexandrusurugiu@gmail.com>
2024-11-18 20:05:30 +0200

    Fragments

---
+++ app/src/main/java/com/example/istoriaalfabetelor/fragments/AlphabetsFragment.java
@@ -0,0 +1,31 @@
+package com.example.istoriaalfabetelor.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.istoriaalfabetelor.R;
+
+
+public class AlphabetsFragment extends Fragment {
+
+    public AlphabetsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alphabets, container, false);
+    }
+}
